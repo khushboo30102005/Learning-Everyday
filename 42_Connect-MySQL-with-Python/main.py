cdad1d419ce6a79890bee13417bf27e6f547cf15
khushboo saini <khushboo30102005@gmail.com>
2025-11-15 21:35:43 +0530

    Connect to MySQL DataBase  with Python

---
+++ 42_Connect-MySQL-with-Python/main.py
@@ -0,0 +1,16 @@
+import mysql.connector
+try:
+  mydb = mysql.connector.connect(host='localhost', user='root', password='Arvind@10799', database='db1')
+  if mydb.is_connected():
+    print('Connected to MySQL DataBase.')
+    cursor = mydb.cursor()
+    cursor.execute('show databases')
+    for x in cursor:
+      print(x)
+  else:
+    print('Not Connected to MySQL DataBase')  
+except mysql.connector.Error as e:
+  print("Error connecting MySQL", e)    
+finally:
+  if mydb.is_connected:
+      cursor.close()
