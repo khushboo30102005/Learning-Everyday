d0ee1018e56360afaf0a41a832d6be9dd405753b
khushboo saini <khushboo30102005@gmail.com>
2025-11-16 22:51:01 +0530

    Fetch data from MySQL Database

---
+++ 43_Fetch-MySQL-Data-With-Python/main.py
@@ -0,0 +1,23 @@
+import mysql.connector
+def fetchEmployee(cursor):
+  cursor.execute('select * from employee')
+  return list(cursor)
+def main():
+  try:
+    mydb = mysql.connector.connect(host='localhost', user='root', password='Arvind@10799', database='college')
+    if mydb.is_connected():
+      print('MySQL database connected')
+      cursor = mydb.cursor()
+      empList = fetchEmployee(cursor)
+      for record in empList:
+        print(record)
+    else:
+      print('MySQL database is not connected')  
+  except mysql.connector.Error as e:
+    print("Error: ",e)
+  finally:
+    if mydb.is_connected():
+      cursor.close()
+  
+if __name__=='__main__':
+  main()  
