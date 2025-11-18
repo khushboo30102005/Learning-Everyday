f452755b599847b71451b6f998d01f388e8cf891
khushboo saini <khushboo30102005@gmail.com>
2025-11-18 23:11:26 +0530

    update data in MySQL database using Python

---
+++ 45_Update-MySQL-Record/update.py
@@ -0,0 +1,32 @@
+import mysql.connector
+class Employee:
+  def __init__(self, data):
+    self.id, self.Name, self.Manager_id = data
+    
+def updateEmployee(cursor, emp):
+      q = "update employee set Name='{}', Manager_id={} where id={}".format(emp.Name, emp.Manager_id, emp.id)
+      cursor.execute(q)
+
+def main():
+  try:
+    mydb = mysql.connector.connect(host='localhost', user='root', password='Arvind@10799', database='college')
+    if mydb.is_connected():
+      print('Database connect.')
+      cursor = mydb.cursor()
+      emp = Employee((101, 'Jhon Doe', 102))
+      updateEmployee(cursor, emp)
+      mydb.commit()
+    else:
+      print('Connection Failed.')  
+             
+  except mysql.connector.Error as e:
+    print("Error: ", e)
+  finally:
+    if mydb.is_connected():
+      mydb.close()
+        
+      
+if __name__=='__main__':
+  main()      
+  
+  
